db8eed1733eadcc44a0e6503e4493ff2500f3482
strider99 <panigrahiomprakash@gmail.com>
2019-01-07 04:35:01 +0530

    add app structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        title: 'Take out the trash',
+        id: 1,
+        completed: false
+      },
+      {
+        title: 'Sleep well',
+        id: 2,
+        completed: false
+      },
+      {
+        title: 'Eat healthy stuff',
+        id: 3,
+        completed: false
+      }
+    ]
+  }
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>App</h1>
+        <Todos todos={this.state.todos} />
+
       </div>
     );
   }
